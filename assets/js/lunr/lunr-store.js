0ded34ba81f69f876eff76ee446a6703da1fd2d4
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2022-09-22 22:20:43 +0100

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,61 +1,84 @@
-var store = [{
-        "title": "Henry Yip",
-        "excerpt":"Education 2021-2025 BSc Mathematical Physics (Hons), University of Edinburgh I spent an enormous amount of time in these posts. Please read them [Astrophysics Notes] [Chess Series] I have a plethora of interests: Quantum Physics As a Mathematical Physics Major, I study physics with mathematical rigor. Currently, my favourite topic is...","categories": ["Henry"],
-        "tags": [],
-        "url": "/Contributors/HenryYip/",
-        "teaser": "/assets/images/Teaser.png"
-      },{
-        "title": "Leo Chow",
-        "excerpt":"Leo participated in this website by writing a very insightful physics article, with some really artistic illustration. Thank you very much for his contribution!  ","categories": [],
-        "tags": [],
-        "url": "/Contributors/LeoChow/",
-        "teaser": "/assets/images/Teaser.png"
-      },{
-        "title": "Grace Yang",
-        "excerpt":"Grace participated in my STEM project and agreed to feature her essay in my website. Below is her self introduction: Hi! I am Grace, doing medicine at the University of Edinburgh. Although my course mostly focuses on biology, I have always wanted to gain deeper insights into other STEM subjects...","categories": [],
-        "tags": [],
-        "url": "/Contributors/GraceYang/",
-        "teaser": "/assets/images/Teaser.png"
-      },{
-        "title": "Michael So",
-        "excerpt":"Michael participated in my STEM project and agreed to feature his essay in my website. Below is his self introduction: A student who is continuously exploring the world, particularly the world within our cranium, while trying to blur the boundaries of arts and sciences. What I have to clarify in...","categories": [],
-        "tags": [],
-        "url": "/Contributors/MichaelSo/",
-        "teaser": "/assets/images/Teaser.png"
-      },{
-        "title": "Alzheimer’s Disease- an Existential Threat?",
-        "excerpt":"Since the author didn’t put his/her name in the essay, I decided not to reveal it. If you are the author and you want me to show it, please just contact me through whatsapp.   You can download it here     ","categories": [],
-        "tags": [],
-        "url": "/Existential_Risks/Alzheimer/",
-        "teaser": "/assets/images/Brain.png"
-      },{
-        "title": "Dark Energy and the Big RIP",
-        "excerpt":"I spent around 3-4 days writing this short essay, featuring: Introduction to the Cosmic Distance Scale How Supernova Ia lead to the discovery of Dark Energy Properties of Dark Energy A detour to Einstein’s Field Equation (EFE) Consequences of the Dark Energy Timescale of its occurance using precise data Source...","categories": [],
-        "tags": [],
-        "url": "/Existential_Risks/BigRIP/",
-        "teaser": "/assets/images/Astro.png"
-      },{
-        "title": "Biological Weapons and Existential Risk",
-        "excerpt":"This Essay is written by Grace Yang, a very enthusiastic medic student studying in the University of Edinburgh. You can either read the essay below, or download it HERE       ","categories": [],
-        "tags": [],
-        "url": "/Existential_Risks/BioWeapons/",
-        "teaser": "/assets/images/Beaker.png"
-      },{
-        "title": "Prions and Prion Diseases",
-        "excerpt":"This essay is written by Michael So You can download it below:     Word     ","categories": [],
-        "tags": [],
-        "url": "/Existential_Risks/Prions/",
-        "teaser": "/assets/images/Teaser.png"
-      },{
-        "title": "The expansion of the sun into the orbit of Earth",
-        "excerpt":"This essay is written by Terrance Corey. You can download it below:     PDF   Word     ","categories": [],
-        "tags": [],
-        "url": "/Existential_Risks/Sun/",
-        "teaser": "/assets/images/Sun.png"
-      },{
-        "title": "Sad Story",
-        "excerpt":"Hello I still don’t know if I get in Orchestra ah, life is so sad :( :( :/ Ahh 10pm already! Still don’t call me, can’t get in… Oh no tomorrow dynamics class! Can’t watch Ducks while eating bread!   Quack! Quack! Quack!  ","categories": [],
-        "tags": [],
-        "url": "/Welcome/",
-        "teaser": "/assets/images/Wolf.png"
-      }]
+---
+layout: none
+---
+
+var store = [
+  {%- for c in site.collections -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
+    {%- for doc in docs -%}
+      {%- if doc.header.teaser -%}
+        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
+      {%- else -%}
+        {%- assign teaser = site.teaser -%}
+      {%- endif -%}
+      {
+        "title": {{ doc.title | jsonify }},
+        "excerpt":
+          {%- if site.search_full_content == true -%}
+            {{ doc.content | newline_to_br |
+              replace:"<br />", " " |
+              replace:"</p>", " " |
+              replace:"</h1>", " " |
+              replace:"</h2>", " " |
+              replace:"</h3>", " " |
+              replace:"</h4>", " " |
+              replace:"</h5>", " " |
+              replace:"</h6>", " "|
+            strip_html | strip_newlines | jsonify }},
+          {%- else -%}
+            {{ doc.content | newline_to_br |
+              replace:"<br />", " " |
+              replace:"</p>", " " |
+              replace:"</h1>", " " |
+              replace:"</h2>", " " |
+              replace:"</h3>", " " |
+              replace:"</h4>", " " |
+              replace:"</h5>", " " |
+              replace:"</h6>", " "|
+            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+          {%- endif -%}
+        "categories": {{ doc.categories | jsonify }},
+        "tags": {{ doc.tags | jsonify }},
+        "url": {{ doc.url | relative_url | jsonify }},
+        "teaser": {{ teaser | relative_url | jsonify }}
+      }{%- unless forloop.last and l -%},{%- endunless -%}
+    {%- endfor -%}
+  {%- endfor -%}{%- if site.lunr.search_within_pages -%},
+  {%- assign pages = site.pages | where_exp:'doc','doc.search != false' -%}
+  {%- for doc in pages -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+  {
+    "title": {{ doc.title | jsonify }},
+    "excerpt":
+        {%- if site.search_full_content == true -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | jsonify }},
+        {%- else -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+        {%- endif -%}
+      "url": {{ doc.url | absolute_url | jsonify }}
+  }{%- unless forloop.last and l -%},{%- endunless -%}
+  {%- endfor -%}
+{%- endif -%}]
